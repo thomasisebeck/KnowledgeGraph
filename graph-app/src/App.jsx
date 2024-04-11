89d0d173d401481ac385a65be2228701e01c5d3b
thomasisebeck <tisebeck@proton.me>
2024-04-11 14:16:16 +0200

    created graph

--- graph-app/src/App.jsx
+++ graph-app/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
+import Graph from './components/Graph.jsx'
 
 function App() {
-
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Graph/>
+        </>
+    )
 }
 
 export default App
