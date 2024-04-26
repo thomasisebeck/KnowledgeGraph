cef8c8278494d6a85ed821feb615b84b7edd911e
thomasisebeck <tisebeck@proton.me>
2024-04-26 21:54:35 +0200

    nodes how have click events

--- graph-app/src/components/MyNetwork.jsx
+++ graph-app/src/components/MyNetwork.jsx
@@ -1,29 +1,30 @@
-import { Network, Node, Edge } from '@lifeomic/react-vis-network';
-import s from './myNetwork.module.scss'
+import React, { Component , createRef} from "react";
+import { Network, Node, Edge } from "@lifeomic/react-vis-network";
 
-const Decorator = ({label}) => {
-    return (
-        <div
-            onClick={() => console.log(`You clicked ${label}`)}
-            style={s.button}
-        >
-            Click Me
-        </div>
-    );
-};
-
-export default function MyNetwork () {
-    function logMe(label) {
-        console.log(label);
+export default class MyNetwork extends Component {
+    constructor(props) {
+        super(props);
+        this.networkComponent = createRef();
     }
 
-    return (
-        <Network>
-            <Node id="vader" label="Darth Vader" color="white" onclick={() => logMe("Darth Vader")}/>
-            <Node id="luke" label="Luke Skywalker" decorator={Decorator}/>
-            <Node id="leia" label="Leia Organa" decorator={Decorator}/>
-            <Edge id="1" from="vader" to="luke"  label={"hello"}decorator={Decorator}/>
-            <Edge id="2" from="vader" to="leia" decorator={Decorator}/>
-        </Network>
-    )
+    componentDidMount() {
+        this.networkComponent.current.network.on("click", event => {
+            console.log("clicked", event);
+        });
+    }
+
+    render() {
+        return (
+            <Network
+                options={{ width: "100%", height: "300px" }}
+                ref={this.networkComponent}
+            >
+                <Node id="vader" label="Darth Vader" />
+                <Node id="luke" label="Luke Skywalker" />
+                <Node id="leia" label="Leia Organa" />
+                <Edge id="1" from="vader" to="luke" label={"associated with"}/>
+                <Edge id="2" from="vader" to="leia" />
+            </Network>
+        );
+    }
 }
