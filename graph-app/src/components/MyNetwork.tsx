18716560b1609115b147eebaf63f7ec7e84ebf40
thomasisebeck <tisebeck@proton.me>
2024-07-30 20:17:09 +0200

    graph styling

--- graph-app/src/components/MyNetwork.tsx
+++ graph-app/src/components/MyNetwork.tsx
@@ -23,6 +23,18 @@ const options= {
             min: 10,
             max: 30
         }
+    },
+    edges: {
+        font: {
+            color: '#dfdfdf',
+            strokeWidth: 0,
+            size: 16,
+            face: 'courier'
+        },
+        color: {
+            color: '#8f7851',
+            highlight: '#bfa684'
+        }
     }
 }
 
@@ -64,7 +76,8 @@ const MyNetwork = ({nodes, relationships, clickEvent} : GraphType) => {
                   const MINIMUM_THICKNESS = 0.4;
 
                   const thickness = (THICKNESS_MULTIPLIER * (sigmoid(r.votes + 1) - 0.5)) + MINIMUM_THICKNESS;
-                  return <Edge id={uniqueKey} from={r.from} to={r.to} label={r.type} width={thickness} arrows={'to'} key={uniqueKey} />
+                  const ARROWS = r.doubleSided ? 'to from' : 'to'
+                  return <Edge id={uniqueKey} from={r.from} to={r.to} label={r.type.replace('_', ' ').toLowerCase()} width={thickness} arrows={ARROWS} key={uniqueKey} />
               })
           }
 
