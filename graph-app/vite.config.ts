880679ee05f1203229b980845ff51df47916d899
thomasisebeck <tisebeck@proton.me>
2024-08-01 00:14:20 +0200

    build

--- graph-app/vite.config.ts
+++ graph-app/vite.config.ts
@@ -1,7 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      pluginPurgeCss()
+  ],
 })
