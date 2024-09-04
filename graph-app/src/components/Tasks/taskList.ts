af791a19f67a326747e82de3aa4870d9aecb3273
thomasisebeck <tisebeck@proton.me>
2024-09-04 17:21:14 +0200

    added questions and answers

--- graph-app/src/components/Tasks/taskList.ts
+++ graph-app/src/components/Tasks/taskList.ts
@@ -15,125 +15,157 @@ post group to classify:    |     1     |     2     |     3     |     4     |
 *** everyone crowdsources ******
 post group to find:        | all but 1 | all but 2 | all but 3 | all but 4 |
 
+PHASE 0: TUTORIAL
+let people learn on a different data set
+have them explore the graph for some time
+have them put information on the graph
+have them upvote and down vote connections
+have them connect 2 nodes
+have them add a category between two nodes
+
+PHASE 1:
+classify 10% of posts in realm 1
+
+---------------------------
+crowdsource
+---------------------------
+
+PHASE 2:
+find 45% of posts not in realm 1
+
+PHASE 3:
+find 45% of posts not in realm 1
+
 */
 
 export const taskList: Task[] = [
-    //Essence is the fundamental nature of a thing, its irreducible core that defines its identity.
-    //Classification: Existence - relates to -> identity - defines -> core nature - associated with -
-    //Title: What is the essence of being
+
+
+    //Consciousness acts as the lens through which we interpret sensory inputs. It filters and organizes incoming information based on our personal experiences, beliefs, and cognitive processes. This subjective processing shapes our perception of reality, highlighting how consciousness and perception are intricately connected.
     {
-        question: "What does essence define?",
-        answer: "Essence defines the irreducible core nature of a thing.",
+        question: "How does consciousness influence perception?",
+        answer: "Consciousness shapes our perception of reality",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "bcf495e8-80ef-44b2-ab5b-90733e35884a",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    // Info: Morality pertains to principles concerning the distinction between right and wrong or good and bad
-    // behavior. Categorization: Ethics - relates to -> behavior - evaluates -> right or wrong - associated with -
-    // Title: What is the foundation of morality?
+    //Time provides the framework within which change occurs, allowing us to measure and understand transformations. The passage of time enables the observation of progress and shifts in states, emphasizing how time is a fundamental dimension in the process of change.
     {
-        question: "How does morality relate to behavior?",
-        answer: "Morality pertains to principles concerning the distinction between right and wrong behavior.",
+        question: "How does time affect the concept of change?",
+        answer: "Time provides a framework in which change occurs",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "7a25822-ffe6-4b46-92fb-7e8690a1ba62",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    // Info: Consciousness is the state of being aware of and responsive to one's surroundings.
-    // Categorization: Existence - is a component of -> being - involves -> awareness - relates to -
-    // Title: What is consciousness?
+    //Intuition plays a crucial role in decision-making by drawing on subconscious knowledge and experiences. It provides immediate, often unspoken guidance that helps individuals make choices without extensive deliberation, relying on patterns and instincts developed over time.
     {
-        question: "What is the definition of consciousness?",
-        answer: "Consciousness is the state of being aware of and responsive to one's surroundings.",
+        question: "What is the role of intuition in decision-making?",
+        answer: "It allows us to draw on subconscious knowledge and past experiences",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "64b5d170-81e2-474a-9a4d-3bc664ba19c6",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    // Info: Collectivity refers to a group of individuals considered as a whole.
-    // Categorization: Culture -> comprises -> groups - relates to - shared characteristics - exhibit
-    // Title: What is a collectivity?
+    //Language is not just a tool for communication but also a structuring force for thought. The vocabulary and grammatical frameworks of a language influence how we conceptualize and understand ideas, thus shaping our cognitive processes and the way we perceive the world.
     {
-        question: "How is a collectivity formed?",
-        answer: "A collectivity is a group of individuals considered as a whole.",
+        question: "How does language shape thought processes?",
+        answer: "It forces us to structure our thoughts and conceptualise and understand our ideas",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "5350160c-9686-4ba2-bfd5-a95a4ed86da2",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    // Info: Communication is the imparting or exchanging of information by speaking, writing, or signaling.
-    // Categorization: Culture - enables -> exchange -> fosters -> connection - relies on ->
-    // Title: What is communication?
+    //Ambiguity introduces the possibility of multiple interpretations and perspectives. This open-endedness can enrich understanding by allowing various viewpoints and meanings to emerge, though it also requires careful analysis to ensure accurate comprehension.
     {
-        question: "How is information exchanged?",
-        answer: "Communication is the imparting or exchanging of information by speaking, writing, or signaling.",
+        question: "What is the significance of ambiguity in interpretation?",
+        answer: "Ambiguity allows for multiple interpretations and perspectives",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "22c49e8f-2bdc-45a7-9e14-52bc2adbd990",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    //Info: Cognition is the mental process of acquiring knowledge and understanding through thought, experience, and the senses.
-    // and the senses. Categorization: Logic -> requires -> thought -> leads to -> cognition - definition ->
-    // Title: What is cognition?
+    //Existentialism explores the meaning of life through the lens of individual freedom and responsibility. It asserts that meaning is not given but created through personal choices and authentic living, emphasizing that each person must find their own path and purpose.
     {
-        question: "How is knowledge acquired according to cognition?",
-        answer: "Cognition is the mental process of acquiring knowledge and understanding through thought, experience, and the senses.",
+        question: "How does existentialism address the meaning of life?",
+        answer: "It explores the meaning of life through the lens of individual freedom and responsibility. Each person must find their own path and purpose.",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "31fa1c4d-be98-41df-89d0-de2f7fce6aa5",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    // Info: An economy is a system of production and exchange of goods and services.
-    // Categorization: Culture -> requires -> system -> based on -> production -> supports ->
-    // Title: What is an economy?
+    //Uncertainty is a driving force in scientific inquiry as it highlights the limits of current knowledge and stimulates further research. The recognition of uncertainty encourages scientists to test hypotheses, gather data, and refine theories to expand understanding.
     {
-        question: "How are goods and services exchanged in an economy?",
-        answer: "An economy is a system of production and exchange of goods and services.",
+        question: "Why is uncertainty useful in scientific research",
+        answer: "It encourages scientists to test hypotheses, gather data and expand understanding",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "8f18613a-d92e-409e-ac0d-95a62582861d",
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
     },
 
-    // Info: A pattern is a recurring characteristic or event.
-    // Categorization: Nature <- exists in - recurrence - aids -> prediction - leads to ->
-    // Title: What is a pattern?
+    //Justice and equality are interrelated concepts in societal contexts. Justice seeks to ensure fair distribution of resources and opportunities, while equality focuses on treating individuals the same. Together, they address how societies can balance fairness with equal treatment.
     {
-        question:
-            "How is a pattern defined in terms of characteristics or events?",
-        answer: "A pattern is a recurring characteristic or event.",
+        question: "Why is equality considered in tandem when considering equity?",
+        answer: "They must be considered together when addressing fairness and equal treatment",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "8f18613a-d92e-409e-ac0d-95a62582861d",
+        targetNodeId: "TO FILL OUT",
+        clicksTillInNeighborhood: 0,
+        totalClicks: 0,
+        username: ""
+    },
+
+    //Free will and determinism represent opposing views on human agency. Free will suggests that individuals can make independent choices, while determinism posits that all events are predetermined by prior causes. The intersection of these ideas raises questions about the nature of autonomy and causality
+    {
+        question: "How does the concept of free will intersect with determinism?",
+        answer: "They are opposing view on human agency",
+        providedAnswer: null,
+        totalTime: 0,
+        expandedNodesPerClick: [],
+        targetNodeId: "TO FILL OUT",
+        clicksTillInNeighborhood: 0,
+        totalClicks: 0,
+        username: ""
+    },
+
+    //Our aesthetic experiences can evoke emotional responses, inspire creativity, and reflect cultural values, thus deeply impacting our interactions with the world. Aesthetics plays a significant role in shaping human experience by influencing how we perceive and appreciate beauty and art.
+    {
+        question: "In what way does aesthetics influence human experience?",
+        answer: "It influences how we perceive and communicate beauty and art",
+        providedAnswer: null,
+        totalTime: 0,
+        expandedNodesPerClick: [],
+        targetNodeId: "TO FILL OUT",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: ""
