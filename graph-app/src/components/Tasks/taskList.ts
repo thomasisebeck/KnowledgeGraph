5aa875b8fe276cbfd09b202970893fce4d2cdc93
thomasisebeck <tisebeck@proton.me>
2024-10-22 17:15:00 +0200

    added new tasks

--- graph-app/src/components/Tasks/taskList.ts
+++ graph-app/src/components/Tasks/taskList.ts
@@ -40,130 +40,70 @@ find 45% of posts not in realm 1
 
 export const taskList: Task[] = [
 
-    //The two normal states of awareness are consciousness and unconsciousness. Higher states of consciousness are often associated with spiritual or mystical experiences. It involves an elevated state of awareness where people are able to gain a greater sense of themselves, their role, and the world. Examples of this include transcendence, meditation, mindfulness, a "runner's high," lucid dreaming, and flow states.
-    //classification: existence <- relates to -> awareness <- synonyms -> consciousness - gives rise to -> states of awareness and sense of self
     {
-        question: "What is a benefit of achieving a higher state of consciousness?",
-        answer: "The two normal states of awareness are consciousness and unconsciousness. Higher states of consciousness are often associated with spiritual or mystical experiences. It involves an elevated state of awareness where people are able to gain a greater sense of themselves, their role, and the world. Examples of this include transcendence, meditation, mindfulness, a \"runner's high,\" lucid dreaming, and flow states.",
+        question: "Into what Parts did Plato decide to divide the soul into?",
+        answer: "Plato believed that the soul is immortal and exists independently of the body. He divided the soul into three parts: the rational part, the spirited part, and the appetitive part. A well-balanced soul, with the rational part in control, is essential for a virtuous life.",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "fe61af91-79b9-4263-917e-2a86a1410745",
+        targetNodeId: "3422f038-2b9b-4125-8af6-e68a9dbb5a48",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: "",
         linkLabels: false
     },
 
-    //Intuition plays a crucial role in decision-making by drawing on subconscious knowledge and experiences. It provides immediate, often unspoken guidance that helps individuals make choices without extensive deliberation, relying on patterns and instincts developed over time.
     {
-        question: "What is the role of intuition in decision-making?",
-        answer: "It allows us to draw on subconscious knowledge and past experiences",
+        question: "What physical law of nature can act as an explanation for time moving only in one direction?",
+        answer: "Why does time flow in one direction, from past to future? This phenomenon is closely related to the second law of thermodynamics, which states that entropy (a measure of disorder) always increases over time.",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
+        targetNodeId: "eb007c64-5de9-490c-ad0f-c79e37e73266",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: "",
         linkLabels: false
     },
 
-    //Language is not just a tool for communication but also a structuring force for thought. The vocabulary and grammatical frameworks of a language influence how we conceptualize and understand ideas, thus shaping our cognitive processes and the way we perceive the world.
     {
-        question: "How does language shape thought processes?",
-        answer: "It forces us to structure our thoughts and conceptualise and understand our ideas",
+        question: "Which theory argues that any action is intrinsically right or wrong, regardless of the consequences?",
+        answer: "Deontology is another ethical theory that emphasizes duty and obligation. It argues that certain actions are inherently right or wrong, regardless of their consequences.",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
+        targetNodeId: "03aea7c9-40d9-425c-96e6-024deddd786f",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: "",
         linkLabels: false
     },
 
-    //Ambiguity introduces the possibility of multiple interpretations and perspectives. This open-endedness can enrich understanding by allowing various viewpoints and meanings to emerge, though it also requires careful analysis to ensure accurate comprehension.
     {
-        question: "What is the significance of ambiguity in interpretation?",
-        answer: "Ambiguity allows for multiple interpretations and perspectives",
+        question: "What phenomenon occurs when a person feels the effect of a treatment based on the belief that it is working, rather than the physical results?",
+        answer: "The placebo effect is a fascinating phenomenon in which a person's belief in a treatment can influence its effectiveness, even if the treatment is ineffective. It demonstrates the powerful role of the mind in shaping our physical and psychological experiences.",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
+        targetNodeId: "6e556c08-b78f-4cfe-99a0-16a21b17cd09",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: "",
         linkLabels: false
     },
 
-    //Existentialism explores the meaning of life through the lens of individual freedom and responsibility. It asserts that meaning is not given but created through personal choices and authentic living, emphasizing that each person must find their own path and purpose.
+    //Existentialism explores the meaning of life through the lens of individual freedom and responsibility. It asserts that meaning is not given but created through personal choices and authentic living, emphasizing that each person must find their own path and purpose.,
     {
-        question: "How does existentialism address the meaning of life?",
-        answer: "It explores the meaning of life through the lens of individual freedom and responsibility. Each person must find their own path and purpose.",
+        question: "What effect does language have on the way we think?",
+        answer: "Language is not just a tool for communication but also a structuring force for thought. The vocabulary and grammatical frameworks of a language influence how we conceptualize and understand ideas, thus shaping our cognitive processes and the way we perceive the world.",
         providedAnswer: null,
         totalTime: 0,
         expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
+        targetNodeId: "0768798-3540-478c-9f12-e346f028679e",
         clicksTillInNeighborhood: 0,
         totalClicks: 0,
         username: "",
         linkLabels: false
     },
 
-    //Uncertainty is a driving force in scientific inquiry as it highlights the limits of current knowledge and stimulates further research. The recognition of uncertainty encourages scientists to test hypotheses, gather data, and refine theories to expand understanding.
-    {
-        question: "Why is uncertainty useful in scientific research",
-        answer: "It encourages scientists to test hypotheses, gather data and expand understanding",
-        providedAnswer: null,
-        totalTime: 0,
-        expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
-        clicksTillInNeighborhood: 0,
-        totalClicks: 0,
-        username: "",
-        linkLabels: false
-    },
-
-    //Justice and equality are interrelated concepts in societal contexts. Justice seeks to ensure fair distribution of resources and opportunities, while equality focuses on treating individuals the same. Together, they address how societies can balance fairness with equal treatment.
-    {
-        question: "Why is equality considered in tandem when considering equity?",
-        answer: "They must be considered together when addressing fairness and equal treatment",
-        providedAnswer: null,
-        totalTime: 0,
-        expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
-        clicksTillInNeighborhood: 0,
-        totalClicks: 0,
-        username: "",
-        linkLabels: false
-    },
-
-    //Free will and determinism represent opposing views on human agency. Free will suggests that individuals can make independent choices, while determinism posits that all events are predetermined by prior causes. The intersection of these ideas raises questions about the nature of autonomy and causality
-    {
-        question: "How does the concept of free will intersect with determinism?",
-        answer: "They are opposing view on human agency",
-        providedAnswer: null,
-        totalTime: 0,
-        expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
-        clicksTillInNeighborhood: 0,
-        totalClicks: 0,
-        username: "",
-        linkLabels: false
-    },
-
-    //Our aesthetic experiences can evoke emotional responses, inspire creativity, and reflect cultural values, thus deeply impacting our interactions with the world. Aesthetics plays a significant role in shaping human experience by influencing how we perceive and appreciate beauty and art.
-    {
-        question: "In what way does aesthetics influence human experience?",
-        answer: "It influences how we perceive and communicate beauty and art",
-        providedAnswer: null,
-        totalTime: 0,
-        expandedNodesPerClick: [],
-        targetNodeId: "TO FILL OUT",
-        clicksTillInNeighborhood: 0,
-        totalClicks: 0,
-        username: "",
-        linkLabels: false
-    },
 ];
